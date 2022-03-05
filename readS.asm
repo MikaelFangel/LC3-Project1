9ef79c66437e3d2835a14fd9d367d2301daec383
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-03-05 22:34:29 +0100

    fixed callee save strategy

--- readS.asm
+++ readS.asm
@@ -5,34 +5,36 @@
 ;   Output: the number read from the console in R0
 ;
                 .ORIG       x4000
-READS           LEA         R1,INPUT
-                ST          R2,Save1                                ; Save registers that are need by routine
+READS           ST          R1,Save1                                ; Save registers that are need by routine
                 ST          R2,Save2
-                ST          R2,Save3
+                ST          R3,Save3
+                ST          R4,Save4
+                LEA         R1,INPUT
                 AND         R1,R1,#0
                 AND         R2,R2,#0
                 LD          R3,ASCII
                 PUTS                                                ; Prints the string labelled input to the console
                 IN
                 ADD         R1,R0,R3                                ; Moves the ASCII value to R1 and converts to bin
-                LEA         R6,MULTR1
-                JSRR        R6
+                LEA         R4,MULTR1
+                JSRR        R4
                 IN
                 ADD         R2,R0,R3                                ; Moves the ASCII value to R2 and converts to bin
 ;
                 AND         R0,R0,#0                                ; Prepares R0 to store the result
                 ADD         R0,R1,R2                                ; Stores the result to R0 
-                
-                LD          R2,Save3                                ; Restore original register values 
+;                
+                LD          R4,Save4                                ; Restore original register values 
+                LD          R3,Save3
                 LD          R2,Save2
                 LD          R1,Save1
                 HALT
 ;
-MULTR1          AND         R0,R0,#0                                ; Multiplies the value in R1 with 10
-                AND         R6,R6,#0
-                ADD         R6,R1,#0
+MULT10R1        AND         R0,R0,#0                                ; Multiplies the value in R1 with 10
+                AND         R4,R4,#0
+                ADD         R4,R1,#0
                 ADD         R0,R0,#9
-AGAIN           ADD         R1,R1,R6
+AGAIN           ADD         R1,R1,R4
                 ADD         R0,R0,#-1
                 BRp         AGAIN
                 RET       
@@ -42,4 +44,5 @@ ASCII           .FILL       xFFD0                                   ; To account
 Save1           .FILL       x0000
 Save2           .FILL       x0000
 Save3           .FILL       x0000
+Save4           .FILL       x0000
                 .END
