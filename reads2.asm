a1d4d8be34c4527de0c5118af691c2c4b398bd79
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-03-16 14:37:59 +0100

    implemented stack storage for the five digits

--- reads2.asm
+++ reads2.asm
@@ -8,14 +8,29 @@
                 .ORIG       x4000
 READS           LEA         R0,INPUT
                 LD          R1,ASCII
+                LD          R6,STACK
                 PUTS                                                ; Prints the string labelled input to the console
+                AND         R2,R2,#0
+                ADD         R2,R2, #5
 NEWIN           IN
-                ADD         R2,R0,#-10                              ; Check if newline was pressed
-                BRnp        NEWIN
+                ADD         R3,R0,#-10                              ; Check if newline was pressed
+                BRz         ENDLOOP
+                
+                ADD         R3,R0,R1                                ; Converts from ASCII to binary
+                ADD         R6,R6,#-1
+                STR         R3,R6,#0                                ; Stores in a simple stack
+                
+                ADD         R2,R2,#-1                               ; Ensures a maximum of five digits is entered
+                BRz         ENDLOOP
+                BRnzp       NEWIN
+                
+ENDLOOP         RET
                 
                 
 RETURN          RET
 
 INPUT           .STRINGZ    "Input a 2 digit decimal number (end with newline):"
 ASCII           .FILL       xFFD0                                   ; To account for difference between ascii and 2's complement
+EMPTY           .FILL       xC000
+STACK           .FILL       x4000
                 .END
