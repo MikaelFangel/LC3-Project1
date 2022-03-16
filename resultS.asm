b887547cea5650dd262a4a2d59df813861bcf6b7
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-03-16 10:47:20 +0100

    added callee save strategy

--- resultS.asm
+++ resultS.asm
@@ -5,15 +5,17 @@
 ;   Output: String to the console
 ;
                 .ORIG       x4300
-                ADD         R0,R0,#0
+                ST          R0,Save1
                 BRp         TRUE
                 LEA         R0,RESULT0
                 BRnzp       RETURN
 ;
 TRUE            LEA         R0,RESULT1
 RETURN          PUTS
+                LD          R0, Save1
                 RET
 ;
 RESULT1         .STRINGZ    "The number is prime"
 RESULT0         .STRINGZ    "The number is not prime"
+Save1           .FILL       x0000
                 .END
