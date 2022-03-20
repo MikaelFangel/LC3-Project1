f083f977346c2f41a7223bd3288adac0b7ca9b99
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-03-20 10:06:43 +0100

    removed halt and added missing comma

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 .ORIG x3000
 
 MAIN	    AND R0, R0, #0
-            AND R1, R1 #0
+            AND R1, R1, #0
             AND R2, R2, #0
             AND R3, R3, #0
             AND R4, R4, #0
@@ -17,8 +17,6 @@ MAIN	    AND R0, R0, #0
             LD R4, RESULTS
             JSRR R4
             BRnzp MAIN
-    
-            HALT
     
 READS       .FILL x4000
 ISPRIME     .FILL x4200
